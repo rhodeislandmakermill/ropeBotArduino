b194558999e6cc66d95dc9308136d11dc84ab607
kellyegan <kelly.egan@gmail.com>
2018-03-26 13:39:53 -0400

    Add description of functionality to README

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Ropebot code for Arduino Due
 
 Code for Arduino Due running game controllers for Ocean State Maker Mill Ropebot race.
+
+The Due controls:
+ * A starter button, 
+ * Two up down controllers for players, 
+ * Two motors through Sparkfun HBridge breakout board for the bots 
+ * Communicates with Raspberry Pi which manages the race and displays results.
--- ropeBotDue.ino
+++ ropeBotDue.ino
@@ -1,3 +1,14 @@
+//  RopeBot Due
+//
+//  Code for Arduino Due running game controllers for Ocean State Maker Mill Ropebot race.
+//
+//  The Due controls:
+//  * A starter button, 
+//  * Two up down controllers for players, 
+//  * Two motors through Sparkfun HBridge breakout board for the bots 
+//  * Communicates with Raspberry Pi which manages the race and displays results.
+
+
 void setup() {
   // put your setup code here, to run once:
 
