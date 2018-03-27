92e6b76df33ffa312ed2397ff5be7fbef2c0515e
kellyegan <kelly.egan@gmail.com>
2018-03-26 20:29:02 -0400

    Add constants for pin assignments

--- ropeBotDue.ino
+++ ropeBotDue.ino
@@ -8,13 +8,70 @@
 //  * Two motors through Sparkfun HBridge breakout board for the bots 
 //  * Communicates with Raspberry Pi which manages the race and displays results.
 
+// Player One button and end stop pins
+const int PLAYER_ONE_DOWN = 10;
+const int PLAYER_ONE_UP = 11;
+const int PLAYER_ONE_TOP = 12;
+const int PLAYER_ONE_BOTTOM = 13;
+
+// Start button pin
+const int START_BUTTON = 14;
+
+// Player TWO button and end stop pins
+const int PLAYER_TWO_DOWN = 15;
+const int PLAYER_TWO_UP = 16;
+const int PLAYER_TWO_TOP = 17;
+const int PLAYER_TWO_BOTTOM = 18;
+
+// Motor pins
+const int PWMB = 2;
+const int BIN2 = 3;
+const int BIN1 = 4;
+const int STBY = 5;
+const int AIN1 = 6;
+const int AIN2 = 7;
+const int PWMA = 8;
+
+//Raspberry Pi pins
+const int PLAYER1 = 47;
+const int PLAYER2 = 49;
+const int START = 51;
+const int RESET = 53;
+
+enum RaceState {
+	initiating,
+	begun,
+	complete
+};
+
+enum PlayerState {
+	waiting,
+	started,
+	halfway,
+	finished
+};
+
+volatile RaceState raceState;
+volatile PlayerState playerOneState;
+volatile PlayerState playerTwoState;
+volatile int winner;
 
 void setup() {
-  // put your setup code here, to run once:
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
+
+void initializeRace() {
+	playerOneState = waiting;
+	playerTwoState = waiting;
+	raceState = begun;
+}
+
+
+
+
+
