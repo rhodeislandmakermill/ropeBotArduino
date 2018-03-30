4f3d623264e48b7e0f619c98bb18b7d8c5cea2fe
kellyegan <kelly.egan@gmail.com>
2018-03-30 09:07:17 -0400

    Created basic parameters for player and initilizer

--- player.cpp
+++ player.cpp
@@ -1,1 +1,12 @@
 #include "player.h"
+
+Player::Player(int top, int bottom, int out, int up, int down ) {
+	topStopPin = top;
+	bottomStopPin = bottom;
+	outPin = out;
+	upButtonPin = up;
+	downButtonPin = down;
+
+	state = offmark;
+}
+
--- player.h
+++ player.h
@@ -1,6 +1,26 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+class Player {
+	public:
+		enum PlayerState {
+			offmark,
+			onmark,
+			started,
+			halfway,
+			finished
+		};
+	
+		Player(int top, int bottom, int out, int up, int down );
+		
 
+	private:
+		int topStopPin;
+		int bottomStopPin;
+		int outPin;
+		int upButtonPin;
+		int downButtonPin;
+		PlayerState state;
+};
 
 #endif
